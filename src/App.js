54c20a8bc237bc709962ccb6f8103afc8c9c805f
Ghulam-Ahmed <201370195@gift.edu.pk>
2023-08-25 22:17:45 +0500

    initials

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Ghulam Ahmed</h2>
+        <h1>201370195</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
